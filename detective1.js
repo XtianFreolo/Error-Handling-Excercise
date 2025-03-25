fc79dd2c01662f666692889b30571842a4971ff8
XtianFreolo <f60@umbc.edu>
2025-03-25 03:47:36 -0400

    This is the start of Error Handling Project

---
+++ detective1.js
@@ -0,0 +1,17 @@
+alert("hello");
+
+function mysteryOperation ()
+{
+	const outcome = Math.random(); // Generates a random number between 0 and 1.
+
+	if (outcome < 0.5)
+	{
+		console.log("The operation is completed successfully!");
+	}
+	else
+	{
+		throw new Error("The operation is failed mysteriously!");
+	}
+}
+
+
